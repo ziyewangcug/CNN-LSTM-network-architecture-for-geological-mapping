dc6f63ecad7b2497b65959585132c5970c767edb
ziyewangcug <85206135+ziyewangcug@users.noreply.github.com>
2021-11-08 16:07:56 +0800

    Add files via upload

---
+++ CNN-LSTM.py
@@ -0,0 +1,101 @@
+# python code for CNN-LSTM network architecture
+# author: Tong Li,Ziye Wang
+# contact: Ziye Wang (Email: ziyewang@cug.edu.cn),
+
+from __future__ import print_function, division
+from keras.layers import *
+import os
+from keras.optimizers import SGD, Adam
+import numpy as np
+from sklearn import metrics
+import csv
+from keras.models import Sequential
+import pandas as pd
+from pandas import read_csv
+from keras import optimizers
+from tensorflow import random
+import matplotlib.pyplot as plt
+
+imgs = []
+labels = []
+
+img = read_csv('positive training_data.csv', header=None) #input positive training data
+img = img.values
+for i in range(img.shape[0]):
+    imgs.append(img[i, :])
+    labels.append(1)
+img = read_csv('negative training_data.csv', header=None) #input negative training data
+img = img.values
+for i in range(img.shape[0]):
+    imgs.append(img[i, :])
+    labels.append(0)
+imgs = np.asarray(imgs, np.float32)
+data = np.reshape(imgs, [-1, 7, 1])# dimension of input data
+label = np.asarray(labels, np.int32)
+
+base = 32
+
+label_all = read_csv('testing_labels.csv', header=None)#input testing labels:0 or 1
+label_all=label_all.values
+
+model = Sequential()
+model.add(Conv1D(base, 3, strides=1, padding='same', input_shape=(7, 1)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv1D(base, 2, strides=1, padding='same'))
+model.add(Activation('relu'))
+model.add(Conv1D(base*2, 2, strides=1, padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling1D(pool_size=2, padding='same'))
+model.add(Conv1D(base*4, 2, strides=1, padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling1D(pool_size=2, padding='same'))
+model.add(Conv1D(base*4, 2, strides=1, padding='same'))
+model.add(Activation('relu'))
+model.add(LSTM(base*8, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True))
+model.add(Dropout(0.5))
+model.add(Dense(base*8, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+
+
+Adam = Adam(lr=0.00001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)#parameters: leraning rate
+model.compile(loss='binary_crossentropy', optimizer = optimizers.Adam(), metrics=['accuracy'])
+history=model.fit(data, label, batch_size=128, epochs=600, verbose=2, validation_split=0.2)#parameters: batch_size, epochs and the proportion of training and validation sets
+
+
+if os.path.exists('result.csv'):
+    os.remove('result.csv')#output the predict probability
+pre = read_csv('testing_data.csv', header=None)#input testing data
+pre = pre.values
+for i in range(pre.shape[0]):
+    pres = np.asarray(pre[i, :], np.float32)
+    predict = np.reshape(pres, [1, 7, 1])
+    x = model.predict(predict, verbose=2)
+    df = pd.DataFrame(x)
+    df.to_csv('result.csv', mode='a+', header=False)
+
+acc = history.history['accuracy']
+loss = history.history['loss']
+val_acc = history.history['val_accuracy']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+plt.title('Accuracy and Loss')
+plt.plot(epochs, acc, 'red', label='Training acc')
+plt.plot(epochs, loss, 'blue', label='Training loss')
+plt.legend()
+plt.show()
+
+loss=pd.DataFrame(loss)
+loss.to_csv('loss.csv',encoding='utf-8')#save the loss function of training data
+acc=pd.DataFrame(acc)
+acc.to_csv('acc.csv',encoding='utf-8')#save the accuracy of training data
+
+plt.title('Validation accuracy and Loss')
+plt.plot(epochs, val_acc, 'red', label='Validation acc')
+plt.plot(epochs, val_loss, 'blue', label='Validation loss')
+plt.legend()
+plt.show()
+
